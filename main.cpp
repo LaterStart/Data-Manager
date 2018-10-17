1ec764dceb1d90144158b0875db15bf4835fcea3
mmaderic <mateo.maderic@maidea.hr>
2018-10-17 11:50:48 +0200

    Tester file

--- main.cpp
+++ main.cpp
@@ -2,9 +2,12 @@
 #include "dm_1.0_src/XMLparser.h"
 using namespace std;
 
+// TEST FILE
 int main() {
-	XMLparser* parser = new XMLparser("db_config.xml");
-	XMLdocument* cfg = parser->ParseFile();
+	XMLparser parser("db_config.xml");
+	XMLdocument* cfg = parser.ParseFile();
+
+
 
 	return 0;
 }
